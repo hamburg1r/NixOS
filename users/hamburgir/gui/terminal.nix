fbafa693c996a476eef3df927f4db8cd8ded2a6b
Hamburgir <shantnuprofid@gmail.com>
2024-08-06 23:54:07 +0530

    Using foot for new terminal

--- users/hamburgir/gui/terminal.nix
+++ users/hamburgir/gui/terminal.nix
@@ -1,4 +1,5 @@
 {
+	pkgs,
 	...
 }: {
 	imports = [
@@ -7,13 +8,33 @@
 	];
 	programs = {
 		alacritty = {
-			enable = true;
+			enable = false;
 		};
 		wezterm = {
-			enable = true;
+			enable = false;
 		};
 		kitty = {
 			enable = true;
+		};
+		foot = {
+			enable = true;
+			server.enable = true;
+			settings = {
+				main = {
+					# term = "xterm-256color";
+					# font = "FiraCode Nerd Font Mono:size=12.5";
+					# shell = "${pkgs.tmux}/bin/tmux new-session -A -s initial";
+				};
+				mouse = {
+					hide-when-typing = "yes";
+				};
+				bell = {
+					visual = "no";
+				};
+				scrollback = {
+					lines = 500;
+				};
+			};
 		};
 	};
 }
