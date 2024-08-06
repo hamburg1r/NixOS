31cc17ffcab94fdaaf57e484f111274ba1690f75
Hamburgir <shantnuprofid@gmail.com>
2024-08-06 23:55:36 +0530

    Dynamic rofi wayland/x11 session

--- users/hamburgir/gui/utils.nix
+++ users/hamburgir/gui/utils.nix
@@ -1,10 +1,22 @@
-{ pkgs, ... }: {
+{
+	pkgs,
+	...
+}: {
 	home.packages = with pkgs; [
 		# grim
 		wayshot
 		slurp
 		swappy
 		# satty
+		(writeShellScriptBin "rofi" ''
+		if [ "$XDG_SESSION_TYPE" == "wayland" ]
+		then
+			${rofi-wayland}/bin/rofi $@
+		else
+			${rofi}/bin/rofi $@
+		fi
+		'')
+		qbittorrent
 	];
 	programs.zathura.enable = true;
 }
