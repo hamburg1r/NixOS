8d88681e91e98980654f299ddec6f03559846b88
Hamburgir <shantnuprofid@gmail.com>
2024-08-06 23:53:54 +0530

    Using tmux as new multiplexer

--- users/hamburgir/shell/multiplexer.nix
+++ users/hamburgir/shell/multiplexer.nix
@@ -1,17 +1,53 @@
 {
 	config,
+	pkgs,
 	...
 }: {
-	programs = {
-		zellij = {
-			enable = true;
-		};
-	};
-
 	home.file."Zellij" = {
 		enable = config.programs.zellij.enable;
 		source = ./zellij;
 		target = ".config/zellij";
+	};
+
+	programs = {
+		tmux = {
+			enable = true;
+			aggressiveResize = true;
+			baseIndex = 1;
+			disableConfirmationPrompt = true;
+			escapeTime = 0;
+			historyLimit = 5000;
+			keyMode = "vi";
+			mouse = true;
+			# newSession = true;
+			# plugins = with pkgs; [
+			# 	tmuxPlugins.cpu
+			# 	{
+			# 		plugin = tmuxPlugins.resurrect;
+			# 		extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+			# 	}
+			# 	{
+			# 		plugin = tmuxPlugins.continuum;
+			# 		extraConfig = ''
+			# 			set -g @continuum-restore 'on'
+			# 			set -g @continuum-save-interval '60' # minutes
+			# 		'';
+			# 	}
+			# ];
+			customPaneNavigationAndResize = true;
+			resizeAmount = 1;
+			# sensibleOnTop = false;
+			shortcut = "Space";
+			secureSocket = false;
+			# terminal = "screen-256color";
+			tmuxp.enable = true;
+		};
+		zellij = {
+			enable = true;
+			enableBashIntegration = false;
+			enableZshIntegration = false;
+			enableFishIntegration = false;
+		};
 	};
 	stylix.targets.zellij.enable = false;
 }
