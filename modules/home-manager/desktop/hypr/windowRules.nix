c2696735d49de3065d584b21a513cd71fbc94241
Hamburgir <shantnuprofid@gmail.com>
2024-08-19 09:43:23 +0530

    Added hyprland window rules

--- modules/home-manager/desktop/hypr/windowRules.nix
+++ modules/home-manager/desktop/hypr/windowRules.nix
@@ -4,6 +4,7 @@
 	wayland.windowManager.hyprland.settings = {
 		windowrulev2 = [
 			"float, class:(org.qbittorrent.qBittorrent), title:^(?!qBittorrent)(.*)"
+			"float, class:(thunar), title:(File Operation Progress)"
 
 			"move 77% 76%, class:(firefox), title:(Picture-in-Picture)"
 			"size 22% 22%, class:(firefox), title:(Picture-in-Picture)"
@@ -12,11 +13,18 @@
 			"suppressevent fullscreen, class:(firefox), title:(Picture-in-Picture)"
 			"suppressevent maximize, class:(firefox), title:(Picture-in-Picture)"
 
+			"stayfocused, class:(polkit-gnome-authentication-agent-1)"
+			"pin, class:(polkit-gnome-authentication-agent-1)"
+
+			"workspace e+0, class:(footclient)|(foot),"
+
 			"workspace 2, class:(firefox),"
 
 			"workspace 4 silent, class:(discord),"
 
 			"workspace 4 silent, class:(vesktop),"
+
+			"workspace 5 silent, class:(Spotify),"
 
 			"workspace special:easyeffects silent, class:(com.github.wwmm.easyeffects),"
 		];
