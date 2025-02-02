eebc15e9a32d9391d3299839286483cc7916989e
Hamburgir <shantnuprofid@gmail.com>
2025-02-02 16:48:59 +0530

    removed speacialisations

--- users/common/specialisation.nix
+++ users/common/specialisation.nix
@@ -4,24 +4,24 @@
 	lib,
 	...
 }: {
-	specialisation = rec {
-		dark.configuration = lib.mkDefault {
-			stylix.polarity = "dark";
-			gtk = {
-				iconTheme = {
-					package = pkgs.papirus-icon-theme;
-					name = "Papirus-Dark";
-				};
-			};
-		};
-		light.configuration = lib.mkDefault {
-			stylix.polarity = "light";
-			gtk = {
-				iconTheme = {
-					package = pkgs.papirus-icon-theme;
-					name = "Papirus-Light";
-				};
-			};
-		};
-	};
+	# specialisation = rec {
+	# 	dark.configuration = lib.mkDefault {
+	# 		stylix.polarity = "dark";
+	# 		gtk = {
+	# 			iconTheme = {
+	# 				package = pkgs.papirus-icon-theme;
+	# 				name = "Papirus-Dark";
+	# 			};
+	# 		};
+	# 	};
+	# 	light.configuration = lib.mkDefault {
+	# 		stylix.polarity = "light";
+	# 		gtk = {
+	# 			iconTheme = {
+	# 				package = pkgs.papirus-icon-theme;
+	# 				name = "Papirus-Light";
+	# 			};
+	# 		};
+	# 	};
+	# };
 }
