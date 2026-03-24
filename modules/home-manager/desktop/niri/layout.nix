88bd26b886adda38972ba698dac52d30d0c81038
Hamburgir <shantnuprofid@gmail.com>
2026-03-24 16:25:36 +0530

    Update Niri config

--- modules/home-manager/desktop/niri/layout.nix
+++ modules/home-manager/desktop/niri/layout.nix
@@ -3,5 +3,12 @@
 }: {
 	programs.niri.settings.layout = {
 		empty-workspace-above-first = true;
+		gaps = 10;
+		struts = {
+			left = 10;
+			right = 10;
+			bottom = 0;
+			top = 0;
+		};
 	};
 }
