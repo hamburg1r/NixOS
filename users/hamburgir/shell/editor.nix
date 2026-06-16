8a5b53c082c4689027d3470a2774ebced3a1761b
Hamburgir <shantnuprofid@gmail.com>
2026-06-16 12:54:19 +0530

    feat(editor): Update editor configurations (nixvimConfig, JetBrains Idea)

--- users/hamburgir/shell/editor.nix
+++ users/hamburgir/shell/editor.nix
@@ -1,19 +1,21 @@
 {
-	inputs,
-	pkgs,
-	...
+    inputs,
+    pkgs,
+    ...
 }: {
-	home = {
-		packages = [
-			# pkgs.nvf # Saves memory
-			pkgs.neovim
-			inputs.mcp-hub.packages."x86_64-linux".default
-		];
-		shellAliases = {
-			vi = "nvim";
-			vim = "nvim";
-		};
-	};
-	# programs.nvf.enable = false;
-	# programs.nixvim.enable = false;
+    home = {
+        packages = [
+            # pkgs.nvf # Saves memory
+            # pkgs.neovim
+            inputs.nixvimConfig.packages."${pkgs.stdenv.hostPlatform.system}".default
+            # inputs.mcp-hub.packages."x86_64-linux".default
+            pkgs.jetbrains.idea-oss
+        ];
+        shellAliases = {
+            vi = "nvim";
+            vim = "nvim";
+        };
+    };
+    # programs.nvf.enable = false;
+    # programs.nixvim.enable = false;
 }
