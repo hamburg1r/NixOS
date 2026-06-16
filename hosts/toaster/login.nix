d7a229d6cda74947c5338692c8372ec9840d3579
Hamburgir <shantnuprofid@gmail.com>
2026-06-16 12:56:50 +0530

    refactor(toaster): Organize login services and disable GDM in login.nix

--- hosts/toaster/login.nix
+++ hosts/toaster/login.nix
@@ -2,15 +2,25 @@
 	inputs,
 	...
 }: {
-	services.greetd = {
-		enable = false;
-		settings = {
-		  default_session = {
-			command = "${inputs}/bin/greeter";
-		    # command = "${pkgs.zsh}/bin/zsh";
-		    # command = "${pkgs.python3Packages.ipython}/bin/ipython";
-		    # command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
-		  };
+	services = {
+		displayManager = {
+			gdm.enable = false;
+			# sddm = {
+				# enable = true;
+				# theme = "catppuccin-mocha";
+			# };
+			# lightdm.enable = false;
+		};
+		greetd = {
+			enable = false;
+			settings = {
+				default_session = {
+					command = "${inputs}/bin/greeter";
+					# command = "${pkgs.zsh}/bin/zsh";
+					# command = "${pkgs.python3Packages.ipython}/bin/ipython";
+					# command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+				};
+			};
 		};
 	};
 }
